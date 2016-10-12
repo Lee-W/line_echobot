0ed789026f86eab5844989211590e2eb73c5be29
LeeW <cl87654321@gmail.com>
2016-10-12 17:33:12 +0800

    Add STATIC_ROOT to settings

--- line_echobot/settings.py
+++ line_echobot/settings.py
@@ -131,7 +131,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-<<<<<<< HEAD
-=======
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
->>>>>>> 6e2d525... Fix
