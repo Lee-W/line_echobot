c6eab5fc1776c2c82be7ad352218a242850289bf
LeeW <cl87654321@gmail.com>
2016-10-12 17:47:44 +0800

    Add callback url and callback in views (not yet implemented)

--- echobot/views.py
+++ echobot/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+
+@csrf_exempt
+def callback(request):
+    return HttpResponse()
