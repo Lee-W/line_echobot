4e4d5d4b23bebdc7879d3451d979c8b4185a11c1
LeeW <cl87654321@gmail.com>
2016-11-23 20:58:21 +0800

    Replace WebhookParser with WebhookHandler

--- echobot/views.py
+++ echobot/views.py
@@ -2,12 +2,29 @@ from django.conf import settings
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
 
-from linebot import LineBotApi, WebhookParser
+from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
-from linebot.models import MessageEvent, TextSendMessage
+from linebot.models import MessageEvent, TextSendMessage, TextMessage
 
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
-parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
+handler = WebhookHandler(settings.LINE_CHANNEL_SECRET)
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_text_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )
+
+
+@handler.default()
+def default(event):
+    print(event)
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text='Currently Not Support None Text Message')
+    )
 
 
 @csrf_exempt
@@ -17,18 +34,11 @@ def callback(request):
         body = request.body.decode('utf-8')
 
         try:
-            events = parser.parse(body, signature)
+            handler.handle(body, signature)
         except InvalidSignatureError:
             return HttpResponseForbidden()
         except LineBotApiError:
             return HttpResponseBadRequest()
-
-        for event in events:
-            if isinstance(event, MessageEvent):
-                line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(text=event.message.text)
-                )
         return HttpResponse()
     else:
         return HttpResponseBadRequest()
